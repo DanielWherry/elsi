1875550fed148a6ec024ea776092bc9ae979d01e
Daniel Wherry <wherrydaniel@gmail.com>
2014-05-20 15:17:50 -0400

    Creater works, checker still in progress for Array Program

--- intArrayORNL.c
+++ intArrayORNL.c
@@ -5,34 +5,35 @@
 int main(int argc, char ** argv){
 
 	const int SIZE = 100;
+	int* integers;
+	integers = (int *)malloc(SIZE*sizeof(int));
 	
-	if(strncmp(argv[1],"--create",8)){	
+	if(strcmp(argv[1],"--create") == 0){	
 	
-		int * integers;
-		integers = (int *)malloc(SIZE*sizeof(int));
-
 		FILE *outfile;
 
-		outfile = fopen(argv[2],"w");
+		outfile = fopen(argv[2],"wb");
 
 		for(int i = 0; i < SIZE; i++){
 			integers[i] = i;
-			fprintf(outfile,"%d\n", integers[i]);
+			//fwrite(integers, sizeof(int), sizeof(integers),outfile);
 		}
+		fwrite(integers, sizeof(int), SIZE ,outfile);
+		printf("Created\n");
 	
+		free(integers);
 		fclose(outfile);
 		return 0;
-	}
-	
-	if(strncmp(argv[1],"--verify",8)){
+		
+	}else if(strcmp(argv[1],"--verify") == 0){
 
-		int* value;
+		//int* value;
 		FILE *infile;
 	
 		infile = fopen(argv[2],"r");
 
 		for (int i = 0; i < SIZE; i++){
-			if(i != fscanf(infile,"%d",value)){
+			if(i != fgetc(infile)){//fread(integers ,sizeof(int), SIZE, infile)){{
 				printf("The files are not the same!!\n");
 				fclose(infile);
 				return 0;
@@ -40,9 +41,12 @@ int main(int argc, char ** argv){
 		}
 		printf("The files are equivalent!\n");
 		fclose(infile);
+		printf("Verified\n");
 		return 0;
+
 	}	
 	
+	return 0;
 }
 	
 
