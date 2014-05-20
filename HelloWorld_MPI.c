a7c8fa888b6b7de0e98b931be892e288dbddee7f
Daniel Wherry <wherrydaniel@gmail.com>
2014-05-20 13:57:35 -0400

    Changed extensions and all code works

--- HelloWorld_MPI.c
+++ HelloWorld_MPI.c
@@ -3,14 +3,14 @@
 
 int main(int argc, char ** argv){ // or char* argv[] , pointer to a pointer
 
-	int id, ierr, p;
+	int rank, size;
 	
-	ierr = MPI_Init(&argc, &argv);
-	ierr = MPI_Comm_size(MPI_COMM_WORLD, &p);
-	ierr = MPI_Comm_rank(MPI_COMM_WORLD, &id);
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);// Initializes which core is being used
+	MPI_Comm_size(MPI_COMM_WORLD, &size);//Initilizes how many cores there are
 
-	printf("Process %d says 'Hello, World'\n",id);
+	printf("Hello from process %d of %d\n",rank, size);
 	
-	ierr = MPI_Finalize();
+	MPI_Finalize();
 	return 0;
 } 
--- intArrayORNL.c
+++ intArrayORNL.c
@@ -1,22 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char ** argv){
 
-	const int SIZE = 1000000000;
-	int * integers;
-	integers = (int *)malloc(SIZE*sizeof(int));
+	const int SIZE = 100;
+	
+	if(strncmp(argv[1],"--create",8)){	
+	
+		int * integers;
+		integers = (int *)malloc(SIZE*sizeof(int));
 
-	FILE *outfile;
+		FILE *outfile;
 
-	outfile = fopen(argv[2],"w");
+		outfile = fopen(argv[2],"w");
 
-	for(int i = 0; i < SIZE; i++){
-		integers[i] = i;
-		fprintf(outfile,"%d\n", integers[i]);
+		for(int i = 0; i < SIZE; i++){
+			integers[i] = i;
+			fprintf(outfile,"%d\n", integers[i]);
+		}
+	
+		fclose(outfile);
+		return 0;
 	}
-	fclose(outfile);
-	return 0;
+	
+	if(strncmp(argv[1],"--verify",8)){
+
+		int* value;
+		FILE *infile;
+	
+		infile = fopen(argv[2],"r");
+
+		for (int i = 0; i < SIZE; i++){
+			if(i != fscanf(infile,"%d",value)){
+				printf("The files are not the same!!\n");
+				fclose(infile);
+				return 0;
+			}
+		}
+		printf("The files are equivalent!\n");
+		fclose(infile);
+		return 0;
+	}	
+	
 }
 	
 
