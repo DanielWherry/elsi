4ed750c7fdfa4de9d2b41f6a7c93a0b8300c3e53
Daniel Wherry <wherrydaniel@gmail.com>
2014-05-21 10:11:05 -0400

    MPI_Wtime now works for the create block in Array program

--- intArrayORNL.c
+++ intArrayORNL.c
@@ -6,49 +6,81 @@
 int main(int argc, char ** argv){
 	
 	MPI_Init(&argc, &argv);
-	double start, end, startVer, endVer;
-	const int SIZE = 100;
+	double startOpen, endOpen, startArr, endArr, startRoW, endRoW, startClose, endClose;// RoW = Read or Write
+	const int SIZE = 1000;
 	int* integers;
 	integers = (int *)malloc(SIZE*sizeof(int));
 	
 	if(strcmp(argv[1],"--create") == 0){	
-		start = MPI_Wtime();
+	
+		startOpen = MPI_Wtime();// Start timing
 		FILE *outfile;
-
 		outfile = fopen(argv[2],"w");
+		endOpen = MPI_Wtime();// End timing
 
+		startArr = MPI_Wtime();// Start Timing
 		for(int i = 0; i < SIZE; i++){
 			integers[i] = i;
 		}
+		endArr = MPI_Wtime();// End Timing
+		
+		startRoW = MPI_Wtime();// Start Timing
 		fwrite(integers, sizeof(int), SIZE ,outfile);
-		end = MPI_Wtime();
-		printf("Created\n");
+		endRoW = MPI_Wtime();// End Timing
+
+		printf("\nCreated\n");
 	
 		free(integers);
+		
+		startClose = MPI_Wtime();//Start Timing
 		fclose(outfile);
+		endClose = MPI_Wtime();// End Timing
+
+
+		printf("Time taken to open file for writing: %f seconds.\n",endOpen-startOpen);
+		printf("Time taken to create array with %d entries: %f seconds.\n", SIZE, endArr-startArr);
+		printf("Time taken to write entries of the array to the file: %f seconds.\n", endRoW-startRoW);
+		printf("Time taken to close file: %f seconds.\n\n",endClose-startClose);
+		
+
 		return 0;
 		
 	}else if(strcmp(argv[1],"--verify") == 0){
-		start = MPI_Wtime();
-		FILE *infile;
-	
+		
+		startOpen = MPI_Wtime();//Start Timing
+		FILE *infile;	
 		infile = fopen(argv[2],"r");
+		endOpen = MPI_Wtime();// End Timing
+
+		startRoW = MPI_Wtime();//Start Timing
 		fread(integers, sizeof(int), SIZE, infile);
-		end = MPI_Wtime();
+		endRoW = MPI_Wtime();// End Timing
+		
+		startArr = MPI_Wtime();// Start Timing
 		for(int i = 0; i < SIZE; i++){
-			if(integers[i] != i){			
+			if(integers[i] != i){
+				endArr = MPI_Wtime();// End Timing if files not same
+			
 				printf("The files are not the same!!\n");
 				free(integers);
+
+				startClose = MPI_Wtime();// Start timing
 				fclose(infile);
+				endClose = MPI_Wtime();// End Timing
+
 				return 0;
 				
 			}
 		}
-	
+		endArr = MPI_Wtime();// End Timing if files same
 		
 		printf("The files are equivalent!\n");
 		free(integers);
+		
+		startClose = MPI_Wtime();// Start Timing
 		fclose(infile);
+		endClose = MPI_Wtime();// End Timing
+
 		return 0;
 
 	}	
