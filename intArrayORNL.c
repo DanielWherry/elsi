ca3b52264e920dcf9b40771379f356dce441671f
Daniel Wherry <wherrydaniel@gmail.com>
2014-05-27 14:27:38 -0400

    Unecessary variables

--- intArrayORNL.c
+++ intArrayORNL.c
@@ -20,7 +20,7 @@ void printVerifyFile(Timing*, int);
 
 int main(int argc, char ** argv){
 	
-	int rank, numProc, arraySize, lowerBound, upperBound;
+	int rank, numProc, lowerBound, upperBound;
 	
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
