af64c97c8bbbf0257b4a463808b7485365531e12
DanielWherry <wherrydaniel@gmail.com>
2014-06-04 14:50:09 -0400

    Added standard deviation to output for better stats

--- graph.py
+++ graph.py
@@ -7,20 +7,20 @@ import json
 from pylab import *
 
 
-def html(message, openTimeMean, closeTimeMean):
+def html(message, openTimeMean, closeTimeMean, openTimeDev, closeTimeDev):
 	this = open('stripingBenchmarkResults.html', 'w')
 	finalMessage = """<html>
 	<head><title>This is a report</title></head>
 	<body>
 	<table><tr>
 	<td><p align="center"> <img src ="OpenTime.png" alt = "It's closing time..."align=middle></td>
-	<td><font size="5">This graph displays the time it took for each rank to open a file. The average time for opening a file was %(openTime)f seconds.</font></p></td></tr>
+	<td><font size="5">This graph displays the time it took for each rank to open a file. The average time for opening a file was %(openTime)f seconds, with a standard deviation of %(oDev)f seconds.</font></p></td></tr>
 	<tr><td><p align="center"><img src ="CloseTime.png" alt = "It's closing time..."align=middle></td>
-	<td><font size="5">This graph displays the time it took for each rank to close a file.The average time for closing a file was %(closeTime)f seconds.</font></p></td></tr>
+	<td><font size="5">This graph displays the time it took for each rank to close a file.The average time for closing a file was %(closeTime)f seconds, with a standard deviation of %(cDev)f seconds.</font></p></td></tr>
 	%(otherMessage)s
 	</table>
 	</body>
-	</html>""" %{"otherMessage":message, "openTime":openTimeMean, "closeTime":closeTimeMean}
+	</html>""" %{"otherMessage":message, "openTime":openTimeMean, "closeTime":closeTimeMean, "oDev":openTimeDev, "cDev":closeTimeDev}
 	this.write(finalMessage)
 	this.close()
 
@@ -67,8 +67,10 @@ for x in sortedDictionaryList:
    write.append(x['Write Time'])
    choice = "The file is being created"
 
-openTimeMean = np.mean(openT)   
+openTimeMean = np.mean(openT) 
+openTimeDev = np.std(openT)  
 closeTimeMean = np.mean(close)
+closeTimeDev = np.std(close)
 
 
 figure(0)
@@ -89,6 +91,7 @@ pyplot.savefig('CloseTime.png')
 
 if choice == "The file is being verified":
   readTimeMean = np.mean(read)
+  readTimeDev = np.std(read)
   figure(2)
   pyplot.scatter(rank,read, label = 'Read Time')
   pyplot.title('Timing Report')
@@ -98,6 +101,7 @@ if choice == "The file is being verified":
   pyplot.savefig('ReadTime.png')
   
   verifyTimeMean = np.mean(verify)
+  verifyTimeDev = np.std(verify)
   figure(3)
   pyplot.scatter(rank,verify, label = 'Verify Time')
   pyplot.title('Timing Report')
@@ -106,13 +110,14 @@ if choice == "The file is being verified":
   pyplot.legend()
   pyplot.savefig('VerifyTime.png')
   message1 = """<tr><td><img src ="VerifyTime.png" alt = "It's closing time..."align=middle></td>
-<td><font size="5">This graph displays the time it took for each rank to verify a file.The average time it took for a rank to verify a file was %(verifyTime)f seconds.</font></td></tr>
+<td><font size="5">This graph displays the time it took for each rank to verify a file.The average time it took for a rank to verify a file was %(verifyTime)f seconds, with a standard deviation of %(vDev)f seconds.</font></td></tr>
   <tr><td><img src ="ReadTime.png" alt = "It's closing time..."align=middle></td>
-<td><font size="5">This graph displays the time it took for each rank to read a file.The average time it took for a rank to read a file was %(readTime)f seconds.</font></td></tr>""" % {"verifyTime": verifyTimeMean, "readTime":readTimeMean}
-  html(message1, openTimeMean, closeTimeMean)
+<td><font size="5">This graph displays the time it took for each rank to read a file.The average time it took for a rank to read a file was %(readTime)f seconds, with a standard deviation of %(rDev)f seconds.</font></td></tr>""" % {"verifyTime": verifyTimeMean, "readTime":readTimeMean, "rDev":readTimeDev, "vDev":verifyTimeDev}
+  html(message1, openTimeMean, closeTimeMean, openTimeDev, closeTimeDev)
 
 if choice == "The file is being created":
   generateTimeMean = np.mean(generate)
+  generateTimeDev = np.std(generate)
   figure(4)
   pyplot.scatter(rank,generate, label="Generate Time")
   pyplot.title('Timing Report')
@@ -122,6 +127,7 @@ if choice == "The file is being created":
   pyplot.savefig('GenerateTime.png')
 
   writeTimeMean = np.mean(write)
+  writeTimeDev = np.std(write)
   figure(5)
   pyplot.scatter(rank,write, label="Write Time")
   pyplot.title('Timing Report')
@@ -130,8 +136,8 @@ if choice == "The file is being created":
   pyplot.legend()
   pyplot.savefig('WriteTime.png')
   message2 = """<tr><td><img src ="GenerateTime.png" alt = "It's closing time..."align="left" /></td>
-	<td><font size="5">This graph displays the time it took for  each rank to generate an array to make the file.The average time it took for each rank  to generate an array for a file was %(generateTime)f seconds.</font></td></tr>
+	<td><font size="5">This graph displays the time it took for  each rank to generate an array to make the file.The average time it took for each rank  to generate an array for a file was %(generateTime)f seconds, with a standard deviation of %(genDev)f seconds.</font></td></tr>
 <tr><td><p align = "center"><img src ="WriteTime.png" alt = "It's closing time..."align=left /></td>
-<td><font size="5">This graph displays the time it took for each rank to write a file. The average time it took for each rank to  write to a file was %(writeTime)f seconds.</font></td></tr>""" %{"generateTime":generateTimeMean, "writeTime":writeTimeMean}
-  html(message2, openTimeMean, closeTimeMean)
+<td><font size="5">This graph displays the time it took for each rank to write a file. The average time it took for each rank to  write to a file was %(writeTime)f seconds, with a standard deviation of %(wDev)f seconds. </font></td></tr>""" %{"generateTime":generateTimeMean, "writeTime":writeTimeMean, "wDev":writeTimeDev, "genDev":generateTimeDev}
+  html(message2, openTimeMean, closeTimeMean, openTimeDev, closeTimeDev)
 
