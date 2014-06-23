30591f7ff6932937c0cafe58155dbb6fbea92d7f
DanielWherry <wherrydaniel@gmail.com>
2014-06-23 16:41:27 -0400

    Using MPI_Scatter to make some ranks do IO and others generate

--- createFile.c
+++ createFile.c
@@ -14,24 +14,31 @@ void printCreateFile(Timing* t, int rank, char* fileSize){
 //THIS FUNCTION CREATES A FILE WITH INFORMATION DETERMINED BY THE USER AT THE COMMAND LINE 
 void createFile(InfoAboutFile fileInfo, long long int integers[], int rank, long long int lowerBound, int numProc, int numIORanks){
 	
-	int sizeOfSubComm = rank % numIORanks;	
-	int* subComm;	
-
-	if(rank % sizeOfSubComm == 0){  
-		subComm = (int*) malloc(sizeof(int) * sizeOfSubComm);
-	}
-
-		
-
-	MPI_Comm otherSubComm;	
-	MPI_Group worldGroup, subGroup;
-	MPI_Comm_group(MPI_COMM_WORLD, &worldGroup);
-	MPI_Group_incl(worldGroup, sizeOfSubComm, subComm, &subGroup); 
-	MPI_Comm_create(MPI_COMM_WORLD, subGroup, &otherSubComm);
-	
 	double start, end;
 	long long int i;
+	char rankID[4];
 	int err=0;	
+	
+	int sizeOfSubComm = numProc / numIORanks;	
+	int* subCommArray;	
+	MPI_Comm subComm;	
+	MPI_Group worldGroup, subGroup;
+
+	if(rank % sizeOfSubComm == 0){
+	
+		sizeOfSubComm = numProc / numIORanks;
+		subCommArray = (int*) malloc(sizeof(int) * sizeOfSubComm);
+		strcpy(rankID, "root");
+		
+		for(i = rank; i < sizeOfSubComm; i++){
+			subCommArray[i] = i;
+		}
+	MPI_Comm_group(MPI_COMM_WORLD, &worldGroup);
+	MPI_Group_incl(worldGroup, sizeOfSubComm, subCommArray, &subGroup); 
+	MPI_Comm_create(MPI_COMM_WORLD, subGroup, &subComm);
+	}
+
+	
 	Timing timerOfProcesses;
 	
 	long long int sizeAssignedToRank;
@@ -51,8 +58,9 @@ void createFile(InfoAboutFile fileInfo, long long int integers[], int rank, long
 	MPI_Request request[numProc];
 	MPI_Offset disp =   sizeof(long long int) * rank * sizeAssignedToRank;
 
-	if(rank == 0){
+	if(strcmp(rankID,"root")){
 		MPI_File_delete(fileInfo.filename, MPI_INFO_NULL);
+
 
 		start = MPI_Wtime();// Start timing
 		err = MPI_File_open(MPI_COMM_WORLD, fileInfo.filename, MPI_MODE_WRONLY|MPI_MODE_CREATE, MPI_INFO_NULL, &outfile);
@@ -62,23 +70,20 @@ void createFile(InfoAboutFile fileInfo, long long int integers[], int rank, long
 		end = MPI_Wtime();// End timing
 		timerOfProcesses.open = end - start;
 
-		for(i = 0; i < numProc; i++){
-			MPI_Irecv(&integers, sizeAssignedToRank, MPI_LONG_LONG_INT, i+1, 0, MPI_COMM_WORLD, &request[i]);
+		MPI_Scatter(integers, sizeAssignedToRank, MPI_LONG_LONG_INT, integers, sizeAssignedToRank, MPI_LONG_LONG_INT, rank, subComm);	
+		
+		MPI_File_set_view(outfile, disp, MPI_LONG_LONG_INT, MPI_LONG_LONG_INT, "native", MPI_INFO_NULL);
 
-			MPI_File_set_view(outfile, disp, MPI_LONG_LONG_INT, MPI_LONG_LONG_INT, "native", MPI_INFO_NULL);
-
-			start = MPI_Wtime();// Start Timing
-			err = MPI_File_write(outfile, integers, sizeAssignedToRank, MPI_LONG_LONG_INT, &status);
-			if(err){
-				MPI_Abort(MPI_COMM_WORLD, 2);
-			}
-			end = MPI_Wtime();// End Timing
-			timerOfProcesses.readOrWrite = end - start;
-			MPI_Wait(&request[i], MPI_STATUS_IGNORE);
+		start = MPI_Wtime();// Start Timing
+		err = MPI_File_write(outfile, integers, sizeAssignedToRank, MPI_LONG_LONG_INT, &status);
+		if(err){
+			MPI_Abort(MPI_COMM_WORLD, 2);
 		}
+		end = MPI_Wtime();// End Timing
+		timerOfProcesses.readOrWrite = end - start;
+		
 	}
 
-	else{
 		start = MPI_Wtime();// Start Timing
 		#pragma omp parallel for
 			for( i = 0; i < sizeAssignedToRank; i++){
@@ -86,8 +91,7 @@ void createFile(InfoAboutFile fileInfo, long long int integers[], int rank, long
 			}
 		end = MPI_Wtime();// End Timing
 		timerOfProcesses.array = end - start;
-		MPI_Send(&integers, sizeAssignedToRank, MPI_LONG_LONG_INT, 0, 0, MPI_COMM_WORLD);
-	}
+
 
 	start = MPI_Wtime();//Start Timing
 	MPI_File_close(&outfile);
