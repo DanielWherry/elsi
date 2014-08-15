ee510388ef4d8937023ef3dc5044b96866797b16
DanielWherry <wherrydaniel@gmail.com>
2014-08-15 13:36:27 -0400

    Expanding timing info

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ int main(int argc, char ** argv){
 	long long int lowerBound, upperBound;
 
 	MPI_Init(&argc, &argv);
+	double start = MPI_Wtime();
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &numProc);
 
@@ -72,7 +73,9 @@ int main(int argc, char ** argv){
 
 	}
 	
+	double end = MPI_Wtime();
 	MPI_Finalize();	
+	printf("Time of Program: %f\n", end - start);
 	return 0;
 
 }
